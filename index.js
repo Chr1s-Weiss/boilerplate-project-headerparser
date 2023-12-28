6e35cade09d1f28af2e074159d78223c26fbd6e3
Chr1s-Weiss <125989355+Chr1s-Weiss@users.noreply.github.com>
2023-12-28 15:50:38 +0100

    test3

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API Test2' });
+  res.json({ greeting: 'hello API Test3' });
 });
 
 // listen for requests :)
